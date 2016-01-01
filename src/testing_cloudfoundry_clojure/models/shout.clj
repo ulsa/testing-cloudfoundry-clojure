20e00de1c6ed54fae2ecac77f9469e3cd41e8116
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2016-01-01 14:25:23 +0100

    Now sanitizing uri before printing.

--- src/testing_cloudfoundry_clojure/models/shout.clj
+++ src/testing_cloudfoundry_clojure/models/shout.clj
@@ -19,14 +19,17 @@
   [m]
   (get-in m [:elephantsql 0 :credentials :uri]))
 
+(defn- sanitize-uri [uri]
+  (clojure.string/replace uri #":[^:]+@" ":******@"))
+
 (defonce spec (if-let [database-url (env :database-url)]
-                (do (println "DATABASE_URL is set:" database-url)
+                (do (println "DATABASE_URL is set:" (sanitize-uri database-url))
                     database-url)
                 (if-let [vcap-services (env :vcap-services)]
-                  (do (println "VCAP_SERVICES is set:" vcap-services)
+                  (do (println "VCAP_SERVICES is set:" (sanitize-uri vcap-services))
                       (parse-elephantsql (json/parse-string vcap-services true)))
                   (let [default "postgresql://localhost:5432/shouter"]
-                    (do (println "Neither DATABASE_URL nor VCAP_SERVICES set, using default:" default)
+                    (do (println "Neither DATABASE_URL nor VCAP_SERVICES set, using default:" (sanitize-uri default))
                         default)))))
 
 (defn all []
